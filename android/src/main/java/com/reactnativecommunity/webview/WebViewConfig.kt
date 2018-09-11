0d71352a1ea4802949bdb754c7728e2014693415
Dulmandakh <dulmandakh@gmail.com>
2018-09-11 17:03:25 +0800

    refactor WebViewConfig.kt to Kotlin code

--- android/src/main/java/com/reactnativecommunity/webview/WebViewConfig.kt
+++ android/src/main/java/com/reactnativecommunity/webview/WebViewConfig.kt
@@ -6,7 +6,7 @@ import android.webkit.WebView;
  * Implement this interface in order to config your {@link WebView}. An instance of that
  * implementation will have to be given as a constructor argument to {@link RCTWebViewManager}.
  */
-public interface WebViewConfig {
+interface WebViewConfig {
 
-  void configWebView(WebView webView);
+  fun configWebView(webView: WebView)
 }
