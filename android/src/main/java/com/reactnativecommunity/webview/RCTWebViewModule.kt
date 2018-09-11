546b19d454e1d8c7531a5bdca314ad8f61d97bac
Dulmandakh <dulmandakh@gmail.com>
2018-09-11 17:06:24 +0800

    refactor RCTWebViewModule.kt to Kotlin

--- android/src/main/java/com/reactnativecommunity/webview/RCTWebViewModule.kt
+++ android/src/main/java/com/reactnativecommunity/webview/RCTWebViewModule.kt
@@ -1,22 +1,9 @@
-
 package com.reactnativecommunity.webview;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
-public class RCTWebViewModule extends ReactContextBaseJavaModule {
+class RCTWebViewModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-  private final ReactApplicationContext reactContext;
-
-  public RCTWebViewModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
-
-  @Override
-  public String getName() {
-    return "RCTWebView";
-  }
+  override fun getName() = "RCTWebView"
 }
