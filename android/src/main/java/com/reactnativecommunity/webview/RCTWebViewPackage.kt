5591d3d975eef4c6992524de8383b58b015344be
Dulmandakh <dulmandakh@gmail.com>
2018-09-12 04:37:15 +0800

    refactor RCTWebViewPackage.kt to Kotlin

--- android/src/main/java/com/reactnativecommunity/webview/RCTWebViewPackage.kt
+++ android/src/main/java/com/reactnativecommunity/webview/RCTWebViewPackage.kt
@@ -1,29 +1,14 @@
-
 package com.reactnativecommunity.webview;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
-public class RCTWebViewPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RCTWebViewModule(reactContext));
-    }
 
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
+class RCTWebViewPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext) = arrayListOf(
+        RCTWebViewModule(reactContext)
+    )
 
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        RCTWebViewManager viewManager = new RCTWebViewManager();
-        return Arrays.<ViewManager>asList(viewManager);
-    }
+    override fun createViewManagers(reactContext: ReactApplicationContext) = arrayListOf(
+        RCTWebViewManager()
+    )
 }
